a4a957dd611fcb7f3168b6311b5963a864c76db0
FranBarrera <franbarrera6@gmail.com>
2015-02-05 19:45:18 +0100

    add function user_ldap

--- add_user.py
+++ add_user.py
@@ -33,3 +33,36 @@ def create_db(name):
 	cursor.execute("grant all privileges on %s.* to " %name+"%s" %name+" identified by "+"'%s'" %passwd)
 	db.commit()
 	return 'password = %s' %passwd
+
+def user_ldap(name):
+	import ldap
+	from ldap import modlist
+
+	# Open connection
+	l = ldap.initialize("ldap://localhost.example.com:636/")
+
+	# login with user admin
+	l.simple_bind_s("cn=admin,dc=example,dc=com","asdasd")
+
+	# new entry
+	dn="cn=user1,ou=People,dc=example,dc=com" 
+
+	# add attributes
+	attrs = {}
+	attrs['objectclass'] = ['top','posixAccount','account']
+	attrs['cn'] = 'usuario'
+	attrs['uid'] = 'usuario'
+	attrs['uidNumber'] = '2000'
+	attrs['gidNumber'] = '2000'
+	attrs['homeDirectory'] = '/var/www/users/usuario'
+	attrs['userPassword'] = 'asdasd'
+	attrs['loginShell'] = '/bin/bash'
+
+	# Convert dict to ldif
+	ldif = modlist.addModlist(attrs)
+
+	# add entry
+	l.add_s(dn,ldif)
+
+	# disconnect server
+	l.unbind_s()
