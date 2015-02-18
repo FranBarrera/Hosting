6bb0635fb477f29f09c1820ad28fdc4040ea08fe
FranBarrera <franbarrera6@gmail.com>
2015-02-18 18:03:38 +0100

    duplicado delete_zone

--- del_user.py
+++ del_user.py
@@ -11,7 +11,8 @@ delete_directory(name,domain)
 delete_db(name)
 delete_ldap(name)
 delete_zone(domain)
-delete_zone(domain)
 
 os.system('service apache2 restart 1>/dev/null 2>/dev/null')
 os.system('service bind9 restart 1>/dev/null 2>/dev/null')
+
+print "Usuario eliminado"
