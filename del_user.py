416355369310e01209e9949d7c53f01d7b482a1f
FranBarrera <franbarrera6@gmail.com>
2015-02-18 17:22:32 +0100

    añadir fichero del_user

---
+++ del_user.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from del_functions import *
+import sys, os
+
+name=(sys.argv[1])
+domain=(sys.argv[2])
+
+
+delete_directory(name,domain)
+delete_db(name)
+delete_ldap(name)
+delete_zone(domain)
+delete_dns(domain)
+
+os.system('service apache2 restart 1>/dev/null 2>/dev/null')
+os.system('service bind9 restart 1>/dev/null 2>/dev/null')
