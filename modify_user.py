450a72374c774ced023c39d6cfd733749e979b58
FranBarrera <franbarrera6@gmail.com>
2015-02-19 12:04:10 +0100

    añadir user_modify.py

---
+++ modify_user.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from modify_functions import *
+import sys, os
+
+name=(sys.argv[1])
+passwd=(sys.argv[2])
+
+modify_db(name,passwd)
--- templates/bind_template.tpl
+++ templates/bind_template.tpl
@@ -1,3 +1,4 @@
+
 zone "{{domain}}" {
         type master;
         file "{{domain}}";
