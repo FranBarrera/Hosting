743812ce02021eca6b2ec786f2e5c92afb65808f
FranBarrera <franbarrera6@gmail.com>
2015-02-19 18:38:24 +0100

    corregir funcion modify

--- modify_user.py
+++ modify_user.py
@@ -7,7 +7,9 @@ tipo=(sys.argv[1])
 name=(sys.argv[2])
 passwd=(sys.argv[3])
 
-if tipo == '-sql'
+if tipo == '-sql':
 	modify_db(name,passwd)
-elif tipo == '-ftp'
+elif tipo == '-ftp':
 	modify_ldap(name,passwd)
+else:
+	print 'Debe especificar un tipo correcto'
