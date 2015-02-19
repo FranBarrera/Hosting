67de32004e12b106a9f52f3a05fe3ed6dc66de7c
FranBarrera <franbarrera6@gmail.com>
2015-02-19 16:44:38 +0100

    eliminar zonas de usuario

--- del_functions.py
+++ del_functions.py
@@ -19,6 +19,22 @@ def delete_directory(name,domain):
 def delete_zone(domain):
 	os.system('rm /var/cache/bind/%s' %domain)
 
+def delete_dns(domain):
+	fread = open('/etc/bind/named.conf.local','r')
+	lineas = fread.readlines()
+	fread.close()
+
+	fwrite = open('/etc/bind/named.conf.local','w')
+	for linea in lineas:
+		if linea == '# zona de %s\n' % domain
+			indice = lineas.index(linea)
+	for i in lineas[indice-1:indice+4]:
+    	lista.remove(i)
+    
+    fwrite.write(lista)
+    fwrite.write()
+
+
 def delete_ldap(name):
 	os.system('ldapdelete -x -D "cn=admin,dc=example,dc=com" "uid=%s,ou=People,dc=example,dc=com" -w super'%name)
 	os.system('ldapdelete -x -D "cn=admin,dc=example,dc=com" "cn=%s,ou=Group,dc=example,dc=com" -w super' %name)
